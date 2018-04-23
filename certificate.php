75fca2efdb98ee83c6813fe2399d631adf72422e
vikeshbhade <vikeshbhade2@gmail.com>
2018-04-24 01:37:59 +0530

    Add files via upload

---
+++ certificate.php
@@ -0,0 +1,21 @@
+
+<html>
+    <section>
+        <div class="container">
+          <center>
+                <div class="text-center">
+                <img src="../assets/hitachi.png" class="img-responsive" alt="Hitachi-Payment Services"/> <br/>
+                <img src="../assets/hitpay.png" alt="Hitachi-Payment Services"/>
+
+                
+                  <h2 class="titleHead">Certificate Of Completion</h2>
+                  <article><h3>This is to certify that </h3> <br/>
+                      <h4> </h4><br/>
+                    <h3>has successfully cleared the</h3> <br/>
+                    <h2> Information Security Awareness Test. </h2><br/>
+                  </article>
+                </div>
+            <center>
+         </div>
+    </section>
+</html>
